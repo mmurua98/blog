504e4e5614b857a23a89de205c5ae7838579b960
mmurua98 <55469486+mmurua98@users.noreply.github.com>
2021-01-23 12:07:09 -0700

    relations

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    /*Relacion polimorfica*/
+
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*Relacion uno a muchos*/
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
